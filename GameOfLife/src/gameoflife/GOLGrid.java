e3fc951fde7b4bebf19c4283087bbf37f01ec39d
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-03-21 18:24:43 -0400

    Completed untested GOL Grid class

--- GameOfLife/src/gameoflife/GOLGrid.java
+++ GameOfLife/src/gameoflife/GOLGrid.java
@@ -1,4 +1,7 @@
 package gameoflife;
+
+import java.util.Random;
+
 /**
  *
  * @author Marshall Ehlinger
@@ -8,11 +11,11 @@ package gameoflife;
 public class GOLGrid {
     // INSTANCE VARIABLES
     private boolean grid[][];
+    
     final private int DEFAULT_WIDTH = 10;
     final private int DEFAULT_HEIGHT = 10;
-
-    final private int HEIGHT, WIDTH;
     
+    final private int HEIGHT, WIDTH;
     
     // CONSTRUCTORS
     public GOLGrid() {
@@ -20,6 +23,7 @@ public class GOLGrid {
         this.WIDTH = DEFAULT_WIDTH;
         this.HEIGHT = DEFAULT_HEIGHT;
         this.grid = new boolean[WIDTH][HEIGHT];
+        grid = getRandomGrid();
     }
     
     public GOLGrid(int width, int height) {
@@ -27,6 +31,7 @@ public class GOLGrid {
         this.WIDTH = width;
         this.HEIGHT = height;
         this.grid = new boolean[WIDTH][HEIGHT];
+        this.grid = getRandomGrid();
     }
     
     public GOLGrid(boolean startArray[][]) {
@@ -36,25 +41,80 @@ public class GOLGrid {
         this.grid = startArray;
     }
     
-    
+
     // PUBLIC METHODS
     public boolean[] getNeighbors(int x, int y) {
-        // STUB
         // Returns array with state of all neighbors, wrapping around ends of grid
         boolean neighbors[] = new boolean[8];
+        int cellX, cellY;
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == -0) {
+                    // Don't check the cell in question for its own state
+                    continue;
+                }
+                cellX = i;
+                cellY = j;
+                // Wrapping logic, a la pacman
+                if (x < 0) {
+                    cellX += this.WIDTH;
+                }
+                else if (x > this.WIDTH) {
+                    cellX -= this.WIDTH;
+                }
+                if (y < 0) {
+                    cellY += this.WIDTH;
+                }
+                else if (y > this.HEIGHT) {
+                    cellY -= this.HEIGHT;
+                }
+                
+                neighbors[count++] = isCellAlive(cellX, cellY);
+            }
+        }
         return neighbors;
     }
     
     public boolean isCellAlive(int x, int y) {
-        // STUB
         // Returns cell state
-        return false;
+        return this.grid[x][y];
     }
     
     public int countLiveNeighbors(int x, int y) {
-        // STUB
         // Counts true neighbors (out of 8) of cell [x][y]
-        // Checks wraps from opposite extreme sids of the grid
-        return 0;
+        int liveNeighbors = 0;
+        boolean neighbors[] = getNeighbors(x, y);
+        for (int i = 0; i < neighbors.length; i++) {
+            if (neighbors[i]) {
+                liveNeighbors++;
+            }
+        }
+        return liveNeighbors;
+    }
+    
+    private boolean[][] getRandomGrid() {
+        // Returns 2d bool grid sized to current grid's dimensions
+        // For constructors only, at this point
+        boolean[][] newGrid = new boolean[this.WIDTH][this.HEIGHT];
+        Random rand = new Random();
+        for (int c = 0; c < this.WIDTH; c++) {
+            for (int r = 0; r < this.HEIGHT; r++) {
+               newGrid[c][r] = rand.nextBoolean();
+            }
+        }
+        return newGrid;
+    }
+    
+    public int getHeight() {
+        return this.HEIGHT;
+    }
+    
+    public int getWIDTH() {
+        return this.WIDTH;
+    }
+    
+    public boolean[][] getGrid() {
+        return this.grid;
     }
 }
