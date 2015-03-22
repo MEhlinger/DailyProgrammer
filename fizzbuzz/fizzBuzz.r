f61aea87d3dcb8efe81ffb8f320d5ed0bf1eb80f
Marshall Ehlinger <mehlinger333@gmail.com>
2015-03-22 02:38:26 -0400

    Added R version of fizzbuzz

---
+++ fizzbuzz/fizzBuzz.r
@@ -0,0 +1,22 @@
+# FizzBuzz in R
+# By Marshall Ehlinger
+
+fizzBuzz <- function(max) {
+  for (i in 1:max) {
+    line <- "";
+    if ((i %% 3) == 0) {
+      line <- paste(line, "fizz", sep ="");
+    }
+    if (i %% 5 ==0) {
+      line <- paste(line, "buzz", sep ="");
+    }
+    if (line == "") {
+      line <- i;
+    }
+    print(toString(line));
+  }
+}
+
+# TEST
+fizzBuzz(100);
+
