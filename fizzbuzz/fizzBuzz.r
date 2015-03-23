1c682a430b29ad0575875adde73d31d355a34eae
Marshall Ehlinger <mehlinger333@gmail.com>
2015-03-23 10:29:06 -0400

    Improved fizzbuzz.r formatting, slightly. Not OCD, dedication to detail and consistency.

--- fizzbuzz/fizzBuzz.r
+++ fizzbuzz/fizzBuzz.r
@@ -4,10 +4,10 @@
 fizzBuzz <- function(max) {
   for (i in 1:max) {
     line <- "";
-    if ((i %% 3) == 0) {
+    if (i %% 3 == 0) {
       line <- paste(line, "fizz", sep ="");
     }
-    if (i %% 5 ==0) {
+    if (i %% 5 == 0) {
       line <- paste(line, "buzz", sep ="");
     }
     if (line == "") {
