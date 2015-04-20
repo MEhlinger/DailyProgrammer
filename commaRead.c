81285425a74e9343c45a8ecd2d46bc5b90b64120
Marshall Ehlinger <mehlinger333@gmail.com>
2015-04-20 10:21:17 -0400

    commaRead.c: Added support for reading newlines from the CSV

--- commaRead.c
+++ commaRead.c
@@ -13,14 +13,19 @@ int main() {
 	FILE *filePointer = fopen("data.txt", "r");
 	const char delim[2] = ",";
 	char *token;
+	char *newline;
 	char buffer[60];
 	int bufferLen = sizeof buffer / sizeof buffer[0];
 
 	if (filePointer != NULL) {
 		while (fgets(buffer, bufferLen, filePointer) != NULL) {
+			newline = strchr(buffer, '\n');
+			 	if (newline) {
+			 		*newline = ','; /* Remove newline characters */
+			 	}
 			token = strtok(buffer, delim);
 			 while (token != NULL) {
-				printf("%s\n", token);
+				printf("-%s-\n", token);
 				token = strtok(NULL, delim);
 			}
 		}
