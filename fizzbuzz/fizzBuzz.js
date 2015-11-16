29fc4b3f03438edeee01a5e6751f9334a02e1fc1
Marshall Ehlinger <ehlingermd15@mounties.mansfield.edu>
2015-11-15 22:02:30 -0500

    Add javascript fizzbuzz, as unreadable oneliner

---
+++ fizzbuzz/fizzBuzz.js
@@ -0,0 +1,3 @@
+// Obnoxious Javascript Nester Ternary Oneliner Fizzbuzz
+// For fun, please don't judge me based on this one.
+for (i = 1; i <= 100; i++) {console.log((i % 3 === 0 && i % 5 === 0) ? "fizzbuzz" : (i % 3 === 0) ? "fizz" : (i % 5 === 0) ? "buzz" : i);}
